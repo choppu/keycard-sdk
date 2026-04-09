bf041e606679f234d7c332e1244cde9c7823a95f
Ksenia Lebedeva <ksenia@bitgamma.com>
2026-04-09 12:20:42 +0200

    added encodeEthPersonalMessage

--- src/ethereum.ts
+++ src/ethereum.ts
@@ -1,5 +1,6 @@
 import { keccak_256 } from "@noble/hashes/sha3";
 import * as secp256 from '@noble/secp256k1';
+import { CryptoUtils } from "./crypto-utils.ts";
 
 export namespace Ethereum {
   export function toEthereumAddress(publicKey: Uint8Array) : Uint8Array {
@@ -8,4 +9,16 @@ export namespace Ethereum {
     }
     return keccak_256(publicKey.subarray(1)).subarray(12);
   }
+
+  export function encodeEthPersonalMessage(message: string) : Uint8Array {
+  const prefixBytes = CryptoUtils.stringToUint8Array('\x19Ethereum Signed Message:\n');
+  const messageBytes = CryptoUtils.stringToUint8Array(message);
+  const prefixedMessage = new Uint8Array(prefixBytes.byteLength + 1 + messageBytes.byteLength);
+
+  prefixedMessage.set(prefixBytes, 0);
+  prefixedMessage[prefixBytes.length] = messageBytes.byteLength;
+  prefixedMessage.set(messageBytes, prefixBytes.length + 1);
+
+  return prefixedMessage;
+}
 }
