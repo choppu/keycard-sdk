5c616d49e5792bfd695a10fb83c222267f14bd67
Ksenia Lebedeva <ksenia@bitgamma.com>
2026-04-09 12:32:57 +0200

    added getMessageHash

--- src/ethereum.ts
+++ src/ethereum.ts
@@ -3,22 +3,26 @@ import * as secp256 from '@noble/secp256k1';
 import { CryptoUtils } from "./crypto-utils.ts";
 
 export namespace Ethereum {
-  export function toEthereumAddress(publicKey: Uint8Array) : Uint8Array {
-    if(publicKey.length == 33) {
+  export function toEthereumAddress(publicKey: Uint8Array): Uint8Array {
+    if (publicKey.length == 33) {
       publicKey = secp256.Point.fromBytes(publicKey).toBytes(false);
     }
     return keccak_256(publicKey.subarray(1)).subarray(12);
   }
 
-  export function encodeEthPersonalMessage(message: string) : Uint8Array {
-  const prefixBytes = CryptoUtils.stringToUint8Array('\x19Ethereum Signed Message:\n');
-  const messageBytes = CryptoUtils.stringToUint8Array(message);
-  const prefixedMessage = new Uint8Array(prefixBytes.byteLength + 1 + messageBytes.byteLength);
+  export function encodeEthPersonalMessage(message: string): Uint8Array {
+    const prefixBytes = CryptoUtils.stringToUint8Array('\x19Ethereum Signed Message:\n');
+    const messageBytes = CryptoUtils.stringToUint8Array(message);
+    const prefixedMessage = new Uint8Array(prefixBytes.byteLength + 1 + messageBytes.byteLength);
 
-  prefixedMessage.set(prefixBytes, 0);
-  prefixedMessage[prefixBytes.length] = messageBytes.byteLength;
-  prefixedMessage.set(messageBytes, prefixBytes.length + 1);
+    prefixedMessage.set(prefixBytes, 0);
+    prefixedMessage[prefixBytes.length] = messageBytes.byteLength;
+    prefixedMessage.set(messageBytes, prefixBytes.length + 1);
 
-  return prefixedMessage;
-}
+    return prefixedMessage;
+  }
+
+  export function getMessageHash(message: Uint8Array) : Uint8Array {
+    return keccak_256(message);
+  }
 }
