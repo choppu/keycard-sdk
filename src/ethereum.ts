bb87c95174c599a5553aedc0931d28a7b3f3fba6
Ksenia Lebedeva <ksenia@bitgamma.com>
2026-04-09 10:54:47 +0200

    added compressed pubKey to ethAddress

--- src/ethereum.ts
+++ src/ethereum.ts
@@ -1,7 +1,11 @@
 import { keccak_256 } from "@noble/hashes/sha3";
+import * as secp256 from '@noble/secp256k1';
 
 export namespace Ethereum {
   export function toEthereumAddress(publicKey: Uint8Array) : Uint8Array {
+    if(publicKey.length == 33) {
+      publicKey = secp256.Point.fromBytes(publicKey).toBytes(false);
+    }
     return keccak_256(publicKey.subarray(1)).subarray(12);
   }
 }
